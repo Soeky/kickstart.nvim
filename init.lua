36f3ee4cf6fc9562aae795b19daf9c972b5e4654
Soeky <seymeny527@gmail.com>
2024-04-05 23:49:30 +0200

    ocaml

--- init.lua
+++ init.lua
@@ -551,7 +551,8 @@ require('lazy').setup({
         html = {},
         jdtls = {},
         markdown_oxide = {},
-        --java_language_server = {},
+        java_language_server = {},
+        ocamllsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
